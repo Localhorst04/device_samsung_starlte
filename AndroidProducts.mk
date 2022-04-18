b48c9b7f810b3895974c4366cb4a738bdb5efd77
synt4x93 <synt4x.93@gmail.com>
2022-04-18 03:58:50 +0100

    starlte: initial commit
    Change-Id: I97b5b2d9215bec4ac20e7151c7b0091c46e0e7a1

---
+++ AndroidProducts.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2022 The LineageOS Project
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_starlte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_starlte-eng \
+    lineage_starlte-user \
+    lineage_starlte-userdebug
---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2022 The LineageOS Project
+
+DEVICE_PATH := device/samsung/starlte
+
+# Display
+TARGET_SCREEN_DENSITY := 560
+
+# Kernel
+TARGET_KERNEL_CONFIG := exynos9810-starlte_defconfig
+
+# Inherit from the common tree
+include device/samsung/exynos9810-common/BoardConfigCommon.mk
